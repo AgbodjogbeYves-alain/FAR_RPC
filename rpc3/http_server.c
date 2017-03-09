f5c8e89a304dfcc105447938a002a1761b9bd71c
Agbodjogbe Yves-alain <agboniel@yahoo.fr>
2017-03-09 16:05:49 +0100

    modif fichier server

--- rpc3/http_server.c
+++ rpc3/http_server.c
@@ -6,89 +6,71 @@
 
 
 #include "http.h"
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <netdb.h>
 
 
 
 void *
 http_null_1_svc(void *argp, struct svc_req *rqstp)
 {
-	static char * result;
+  static char * result;
 
-	/*
-	 * insert server code here
-	 */
+  /*
+   * insert server code here
+   */
 
-	return (void *) &result;
+  return (void *) &result;
 }
 
 response *
 http_request1_1_svc(data1 *argp, struct svc_req *rqstp)
 {
-	static response  result;
-	char port[8];
-  	char *pos;
-  	char buffer[256];
-  	char root[256] = "public_html";
-  	FILE* fichier = NULL;
-	 /* Renseignement du port d'écoute */
-  	printf("Port d'écoute : ");
-  	fgets(port, 8, stdin);
-  	if ((pos=strchr(port, '\n')) != NULL) *pos = '\0';
-	if(strlen(port)==0){
-     		port[0] = '8';
-     		port[1] = '0';
-  	}
+  static response  result;
+  char port[8];
+    char *pos;
+    char buffer[256];
+    char root[256] = "public_html";
+    FILE* fichier = NULL;
 
     /* Verification de la demande*/
     char *tok = strtok(argp->request, " ");
 
-	if(strcmp(tok, "GET") == 0){
-  		printf("Requete acceptée\n");
-	//On récupère la page demandée dans tok
-  		tok = strtok(NULL, " ");
-  		if(strcmp(tok, "/") == 0){
-       		strcat(tok, "index.html");
-  		}
-  		strcat(root, tok);
-  		printf("Tentative d'ouverture de -%s-\n",root);
+  if(strcmp(tok, "GET") == 0){
+      printf("Requete acceptée\n");
+  //On récupère la page demandée dans tok
+      tok = strtok(NULL, " ");
+      if(strcmp(tok, "/") == 0){
+          strcat(tok, "index.html");
+      }
+      strcat(root, tok);
+      printf("Tentative d'ouverture de -%s-\n",root);
 
-  	}else{
+    }else{
       printf("Requete rejetée\n");
       /* Envoie du message au client */
       char buffer[32] = "Bad Request";
-      result.fd = -1;
-      result.char_read = NULL;
-      result.byte_read_nbr = 0;
+      //result.fd = -1;
+      memcpy(result.char_read,buffer);
+      result.byte_read_nbr = 12;
       return &result;
-	}
-	fichier = fopen(root, "r");
-	result.fd = fgetc(fichier);
-	memcpy(result.char_read,fichier);
-	result.byte_read_nbr = sizeof(fichier);
+  }
+  fichier = fopen(root, "r");
+  //result.fd = fgetc(fichier);
+  memcpy(result.char_read,fichier);
+  result.byte_read_nbr = sizeof(fichier);
  return &result;
 }
 
 response *
 http_request2_1_svc(data2 *argp, struct svc_req *rqstp)
 {
-	static response  result;
+  static response  result;
 
-      		if (argp->fd != -1 || argp->fd != (int) NULL){
-          		printf("Envoi des données...\n");
-          		while(fgetc(result->char_read)-1 != NULL){
-	          
-          		}
-			fclose();
-      		}
+          if (argp->fd != -1 || argp->fd != (int) NULL){
+              printf("Envoi des données...\n");
+              while(fgetc(result->char_read)-1 != NULL){
+              }
+      fclose();
+          }
     
-	return &result;
+  return &result;
 }
